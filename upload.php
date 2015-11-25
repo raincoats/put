2760c7e46381518da4cdac15fd2f1e51a172a711
raincoats <tomshaddock@outlook.com>
2015-11-26 10:32:14 +1100

    added comments and more die messages

--- upload.php
+++ upload.php
@@ -1,16 +1,33 @@
 <?php
 /*
- * real simple uploader
- * 24 nov 2015 at 7:34 pm
+ * put.php
+ * by @reptar_xl
+ * simple multiple file uploader over http
+ *
+ * command line usage
+ *   to upload, for example, /etc/passwd and /etc/resolv.conf to a put.php running on
+ *   localhost, you would do this:
+ *
+ *   $ curl -F 'up[]=@/etc/passwd' -F 'up[]=@/etc/resolv.conf' http://localhost/put.php
+ *
+ *   and if all is good, the output should be something like:
+ *     [OK] 0: passwd                 ------>               passwd 1
+ *     [OK] 1: resolv.conf            ------>            resolv.conf
+ *
  */
 
+
+
+/* don't put this in your public html root unless you're running it
+   on localhost or something. otherwise, anyone can just upload and
+   run any sort of php, etc. this uploader doesn't reject php files */
 define("UPLOAD_DIR", '/www/put/uploads/');
 
-
 if (! isset($_FILES['up'])){
-    die('{}');
+    die('No files specified.');
 }
 
+/* sanitizing */
 function mr_sparkle($in)
 {
     if (preg_match('/(r57\.php|c99\.php)/', $in)){
@@ -32,11 +49,14 @@ function mr_sparkle($in)
 
 $up = $_FILES['up'];
 
-for ($i=0; $i<count($up['name']); $i++){
+for ($i=0; $i<count($up['name']); $i++){ // for each file being uploaded
 
     $name = mr_sparkle($up['name'][$i]);
     $temp = $up['tmp_name'][$i];
 
+    /*  if we have more than one file with a name, we have to
+        rename it before we save it! this loops a maximum of 
+        999 times   */
     if (file_exists($name)){
         $o = 1;
         while (file_exists($name ." ". $o)){
@@ -46,11 +66,11 @@ for ($i=0; $i<count($up['name']); $i++){
         $name = $name ." ". $o;
     }
 
-    move_uploaded_file($temp, $name);
-    @chmod($name, 00600);
+    move_uploaded_file($temp, $name) or die("Could not move upload to upload directory");
+    @chmod($name, 00600) or die ("Could not chmod uploaded file");
 
 
-
+    /* [OK] 1: file                 ------>             file */
     printf("[%s] %d: %-20s   ------>   %20s\n",
         (file_exists($name)? 'OK' : 'ERROR'), $i, $up['name'][$i],
         preg_replace('/\/.*\//', '', $name) );
